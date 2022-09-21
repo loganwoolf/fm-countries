f9c7c5b422aaaac89fd071792080acc3cf7b4e22
Logan Woolf <loganwoolf@gmail.com>
2022-09-20 23:10:29 -0600

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Header } from "./components/header";
 
 export const App = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {});
+
   return <Header />;
 };
---
+++ src/components/card.stories.tsx
@@ -0,0 +1,8 @@
+import { Card } from "./card";
+
+export default {
+  title: Card,
+  component: Card,
+};
+
+export const Loading = () => <Card />;
